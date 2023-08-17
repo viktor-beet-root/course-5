51b9044dd00ddd168a34640681c415ec31913da1
Oksana Umienchuk <neborakoksana@gmail.com>
2023-08-17 23:37:34 +0300

    first structure

---
+++ students/umienchuk_oksana/23_js/js/script.js
@@ -0,0 +1,57 @@
+<script>
+        'use strict';
+        // коментар
+        /* dvdv
+        vdvnu
+        dvvd
+        dvv*/
+
+
+        /**
+         * jsDoc
+         * 
+         * 
+         */
+        console.log(f);
+
+        var f = 2;
+        var f = 33;
+        {
+            var f = 44;
+        }
+        console.log(f);
+        f = f * 3;
+        console.log(f);
+
+        let a = 'sds';
+        let aq = 1;
+        aq = 2;
+        {
+            let a = 2;
+            console.log(aq, a);
+        }
+
+        console.log(a);
+        const PI = Math.PI;
+        const pi = Math.PI;
+
+
+        console.log(PI, pi);
+        const name = 'Oksana';
+        const surname = 'Umienchuk';
+        const myname = 'My name is ' + name + ' ' + surname;
+        console.log(myname);
+
+        //const firstName = 'Viktor'; // [a-zA-Z_$] [a-zA-Z_$0-9]
+
+        // const firstName = prompt('What is your name?', '');
+        // console.log(firstName);
+
+        // const isAcsses = confirm('18 +?');
+        // console.log(isAcsses);
+
+        // + - * /  
+
+        const прізвище = 'Degtiarov';
+        console.log(прізвище);
+    </script>
