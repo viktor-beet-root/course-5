835afa52493691d23d3d9aac1b485c8b5302d26c
Viktor Dehtiarov <viktor.beet.root@gmail.com>
2023-08-21 22:42:43 +0300

    25 js Loop and Conditions

---
+++ main/js/25/build/js/script.js
@@ -0,0 +1,112 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**************************!*\
+  !*** ./src/js/script.js ***!
+  \**************************/
+var nub = -3;
+if (nub === 3) console.log(1);else console.log(2);
+if (nub > 0) {
+  console.log(1);
+} else {
+  console.log(2);
+}
+var d = 2;
+if (d % 2) {
+  console.log('1');
+} else {
+  console.log('2');
+}
+if (d === 2) {
+  console.log('2');
+} else if (d > 30 && !(d % 2) || d === 3) {
+  console.log('2defdfssd');
+} else {
+  console.log('3333333333');
+}
+
+// const myAge = 20;
+
+// switch (myAge) {
+//     case 10:
+//         console.log('10');
+//     case 15:
+//     case 20:
+//         console.log('15,20');
+//         break;
+//     case 40:
+//         console.log('40');
+//         break;
+//     default:
+//         console.log('default');
+// }
+
+//const text = prompt('yes | no?');
+
+//let isAccsess = (text === 'yes') ? '+' : ((text === 'no') ? '-' : undefined);
+
+// switch (text?.toLowerCase()) {
+//     case 'yes':
+//         isAccsess = true;
+//         break;
+//     case 'no':
+//         isAccsess = false;
+//         break;
+// }
+
+//console.log(isAccsess);
+var i = 0;
+var n = 6;
+var message = '';
+
+// while (n > i) {
+//     message = message + '#';
+
+//     i++;
+// }
+
+console.log(message);
+
+// let myAge;
+
+// do {
+//     myAge = +prompt('age?');
+// } while (!myAge || myAge < 0 || myAge > 120);
+
+// console.log(myAge);
+//2 + 2 * 2
+
+var stringText = 'dsfkgjhdklsfghdklsfjghlkjsdq';
+console.log(stringText.length, stringText[0], stringText[stringText.length - 1]);
+for (var w = 0; stringText.length > w; w++) {
+  console.log(stringText[w]);
+}
+var m = '';
+n = 10;
+var countCol = 10;
+for (var _i = 0; n > _i; _i++) {
+  m += _i + 1 + '. ';
+  for (var j = 0; j < countCol; j++) {
+    m += '#';
+  }
+  m += '\n';
+}
+console.log(m);
+
+//#
+//##
+//###
+//####
+//
+
+//      #
+//     ##
+//    ###
+//   ####
+
+//      # #
+//     ## ##
+//    ### ###
+//   #### ####
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
