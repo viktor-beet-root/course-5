02679a9515a1e6fdedc429a82ec40f5924accb4c
Mykhailo Linov <michaellinev@gmail.com>
2023-08-21 22:33:31 -0400

    complete minimum and normal tasks

--- students/linov_mykhailo/24_data-types/src/js/script.js
+++ students/linov_mykhailo/24_data-types/src/js/script.js
@@ -3,13 +3,24 @@
 
 // 1. Виконай додавання 0,1 і 0,2 добийся математично правильної відповіді.
 
+const firstNumber = 0.1;
+const secondNumber = 0.2;
+const sumOfTwoNumbers = ((firstNumber * 10) + (secondNumber * 10)) / 10;
+console.log(sumOfTwoNumbers);
 
 // 2. Виконай додавання рядка "1" і цифри 2 (обидві операнди повинні бути в змінних), добийся математично правильної відповіді.
 
+const string = '1';
+const number = 2;
+const sumOfStringAndNumber = ((+string) * 10 + number * 10) / 10;
+console.log(sumOfStringAndNumber);
 
 // 3. Користувач вказує обсяг флешки в Гб. Програма повинна порахувати скільки файлів розміром в 820 Мб поміщається на флешку.
 
-
+const objectSize = 0.82;
+const userGigabytes = prompt('Please enter size of your flash drive in GB');
+const quantityOfFilesThatFitOnFlashDrive = (Math.floor(userGigabytes / objectSize));
+console.log('You can download ' + quantityOfFilesThatFitOnFlashDrive + ' files on this flash drive.');
 
 
 // Норма
@@ -17,11 +28,20 @@
 
 // 1. Користувач вводить суму грошей в гаманці і ціну однієї шоколадки. Програма виводить скільки шоколадок може купити користувач і скільки здачі у нього залишиться.
 
+const userMoney = prompt("Please enter amount of your money");
+const chocolatePrice = prompt("Now please enter the cost of the chocolate bar");
+const chocolateQuantity = Math.floor(userMoney / chocolatePrice);
+const whatRemains = (userMoney - (chocolateQuantity * chocolatePrice)).toFixed(2);
+console.log('You can buy ' + chocolateQuantity + ' chocolate bars and you will have ' + whatRemains + ' hryvnas left.');
 
 // 2. Запитай у користувача тризначне число і виведи його задом наперед. Для вирішення завдання тобі знадобиться оператор % (залишок від ділення).
 
-
-
+const userNumber = prompt('Please enter a three-digit number');
+const hundreds = (userNumber % 10) * 100;
+const tens = (~~(userNumber / 10) % 10) * 10;
+const singles = (~~(userNumber / 100));
+const reverseUserNumber = hundreds + tens + singles;
+console.log('Here is your number, reversed: ' + reverseUserNumber);
 
 // Максимум
 
