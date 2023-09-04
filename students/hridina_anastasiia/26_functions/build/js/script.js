e05e2994656cdf87c78d8c4a561220ce50aa7400
Anastasia Hridina <homura.nyan@gmail.com>
2023-09-04 18:21:22 +0300

    minimum section

--- students/hridina_anastasiia/26_functions/build/js/script.js
+++ students/hridina_anastasiia/26_functions/build/js/script.js
@@ -6,6 +6,7 @@ var __webpack_exports__ = {};
 // Мінімум
 
 // Напиши всі можливі варіанти створення функцій.
+
 function name(parameter1, parameter2) {}
 ; // Function Declaration
 var func = function func() {}; // Function Expression
@@ -14,6 +15,7 @@ var arrow = function arrow(arg1, arg2, arg3, argN) {
 }; // Arrow Function
 
 // Створи функцію, яка буде виводити кількість переданих їй аргументів.
+
 function param() {
   console.log(arguments.length);
 }
@@ -25,21 +27,20 @@ param(12, 6, 45);
 // 0 - якщо числа рівні.
 
 function diff(a, b) {
-  var result;
   if (a < b) {
-    result = -1;
+    return -1;
   } else if (a > b) {
-    result = 1;
+    return 1;
   } else {
-    result = 0;
+    return 0;
   }
-  return result;
 }
-console.log(diff(2, 3));
+console.log(diff(2, 4));
 console.log(diff(5, 1));
 console.log(diff(7, 7));
 
 // Напиши функцію, яка обчислює факторіал переданого їй числа.
+
 function factorial(n) {
   if (n == 0 || n == 1) {
     return 1;
@@ -51,13 +52,46 @@ console.log(factorial(6));
 
 // Напиши функцію, яка приймає три окремі цифри і перетворює їх в одне число. Наприклад: цифри 1, 4, 9 перетворяться в число 149.
 
+function digits(h, m, j) {
+  if (h, m, j) {
+    return h * 100 + m * 10 + j;
+  }
+}
+console.log(digits(4, 5, 6));
+
 // Напиши функцію, яка приймає довжину і ширину прямокутника і обчислює його площу. 
 //Якщо в функцію передали 1 параметр, то вона обчислює площу квадрата.
+
+function area(x, y) {
+  var areaOfRectangle = x * y;
+  var areaOfSquare = x * x;
+  if (x, y) {
+    return areaOfRectangle;
+  } else if (x, x) {
+    return areaOfSquare;
+  }
+}
+console.log(area(11));
+console.log(area(15, 4));
 
 // Норма
 
 // Напиши функцію, яка перевіряє, чи є передане їй число “досконалим числом”. 
 // Досконале число - це число, яке дорівнює сумі всіх своїх дільників.
+function perfectNumber(number) {
+  var n = 0;
+  for (var i = 1; i <= number / 2; i++) {
+    if (number % i === 0) {
+      n += i;
+    }
+  }
+  if (n === number && n !== 0) {
+    console.log('It is a perfect number');
+  } else {
+    console.log('It is not a perfect number');
+  }
+}
+perfectNumber(28);
 
 // Напиши функцію, яка приймає мінімальне і максимальне значення для діапазону, і виводить тільки ті числа з діапазону, які є досконалими. 
 // Використовуй написану раніше функцію, щоб дізнатися, чи є це число досконалим.
