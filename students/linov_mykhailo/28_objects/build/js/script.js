e0110b8725371c7314f0629226867a5efbb25480
Mykhailo Linov <michaellinev@gmail.com>
2023-09-13 11:40:52 -0400

    norm done

--- students/linov_mykhailo/28_objects/build/js/script.js
+++ students/linov_mykhailo/28_objects/build/js/script.js
@@ -2396,6 +2396,54 @@ car.howMuchTimeItTakes(582);
 // Зміни часу на передану кількість хвилин.
 // Зміни часу на передану кількість годин.
 // Враховуйте, що в останніх 3-х функціях, при зміні однієї частини часу, може змінитися і інша. Наприклад: якщо до часу «20:59:45» додати 30 секунд, то повинно вийти «21:00:15», а не «20:59:75». Також потрібно передбачити можливість того що користувач може передати 150 секунд, або 75 хвилин.
+
+var timeObject = {
+  hours: 0,
+  minutes: 0,
+  seconds: 0
+};
+function setTime(hours, minutes, seconds) {
+  if (hours >= 0 && hours < 24 && minutes >= 0 && minutes < 60 && seconds >= 0 && seconds < 60) {
+    timeObject.hours = hours;
+    timeObject.minutes = minutes;
+    timeObject.seconds = seconds;
+    console.log('Time is set up');
+  } else {
+    console.log('Invalid time values');
+  }
+}
+function displayTime() {
+  var hours = timeObject.hours,
+    minutes = timeObject.minutes,
+    seconds = timeObject.seconds;
+  console.log("Current time: " + hours + ":" + minutes + ":" + seconds);
+}
+function addSeconds(secondsAdd) {
+  var totalSeconds = timeObject.hours * 3600 + timeObject.seconds * 60 + timeObject.seconds + secondsAdd;
+  var newHours = Math.floor(totalSeconds / 3600) % 24;
+  var newMinutes = Math.floor(totalSeconds % 3600 / 60);
+  var newSeconds = totalSeconds % 60;
+  setTime(newHours, newMinutes, newSeconds);
+}
+function addMinutes(minutesAdd) {
+  var totalMinutes = timeObject.hours * 60 + timeObject.minutes + minutesAdd;
+  var newHours = Math.floor(totalMinutes / 60) % 24;
+  var newMinutes = totalMinutes % 60;
+  setTime(newHours, newMinutes, timeObject.seconds);
+}
+function addHours(hoursAdd) {
+  var totalHours = timeObject.hours + hoursAdd;
+  var newHours = totalHours % 24;
+  setTime(newHours, timeObject.minutes, timeObject.seconds);
+}
+setTime(12, 30, 45);
+displayTime();
+addSeconds(50);
+displayTime();
+addMinutes(55);
+displayTime();
+addHours(15);
+displayTime();
 }();
 /******/ })()
 ;
--- students/linov_mykhailo/28_objects/src/js/script.js
+++ students/linov_mykhailo/28_objects/src/js/script.js
@@ -49,7 +49,7 @@ const car = {
         console.log('It would take ' + timeNeeded + ' hours and ' + fuelNeeded + ' liters of fuel');
     },
 
-}
+};
 
 
 car.informationAboutCar();
@@ -66,3 +66,64 @@ car.howMuchTimeItTakes(582);
 // Зміни часу на передану кількість хвилин.
 // Зміни часу на передану кількість годин.
 // Враховуйте, що в останніх 3-х функціях, при зміні однієї частини часу, може змінитися і інша. Наприклад: якщо до часу «20:59:45» додати 30 секунд, то повинно вийти «21:00:15», а не «20:59:75». Також потрібно передбачити можливість того що користувач може передати 150 секунд, або 75 хвилин.
+
+const timeObject = {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+};
+
+function setTime(hours, minutes, seconds) {
+    if (hours >= 0 && hours < 24 && minutes >= 0 && minutes < 60 && seconds >= 0 && seconds < 60) {
+        timeObject.hours = hours;
+        timeObject.minutes = minutes;
+        timeObject.seconds = seconds;
+        console.log('Time is set up');
+    } else {
+        console.log('Invalid time values');
+    }
+
+}
+
+function displayTime() {
+    const { hours, minutes, seconds } = timeObject;
+    console.log("Current time: " + hours + ":" + minutes + ":" + seconds);
+}
+
+function addSeconds(secondsAdd) {
+    const totalSeconds = timeObject.hours * 3600 + timeObject.seconds * 60 + timeObject.seconds + secondsAdd;
+    const newHours = Math.floor(totalSeconds / 3600) % 24;
+    const newMinutes = Math.floor((totalSeconds % 3600) / 60);
+    const newSeconds = totalSeconds % 60;
+    setTime(newHours, newMinutes, newSeconds);
+}
+
+function addMinutes(minutesAdd) {
+    const totalMinutes = timeObject.hours * 60 + timeObject.minutes + minutesAdd;
+    const newHours = Math.floor(totalMinutes / 60) % 24;
+    const newMinutes = totalMinutes % 60;
+    setTime(newHours, newMinutes, timeObject.seconds);
+}
+
+function addHours(hoursAdd) {
+    const totalHours = timeObject.hours + hoursAdd;
+    const newHours = totalHours % 24;
+    setTime(newHours, timeObject.minutes, timeObject.seconds);
+}
+
+
+setTime(12, 30, 45);
+
+displayTime();
+
+addSeconds(50);
+
+displayTime();
+
+addMinutes(55);
+
+displayTime();
+
+addHours(15);
+
+displayTime();
