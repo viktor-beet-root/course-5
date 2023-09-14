d9808691db360895e6b73982aa581542fc6d7f64
Alona Dunaieva <dunaeva4715@gmai.com>
2023-09-14 17:12:31 +0300

    added const for text and extravagantly superfluous +

--- students/dunaieva_alona/24_types_operators/src/js/script.js
+++ students/dunaieva_alona/24_types_operators/src/js/script.js
@@ -14,25 +14,28 @@ const askGB = +prompt('What is your flash drive capacity in GB?');
 const driveMB = 820;
 const driveGBinMB = 1024;
 const numberOfFiles = ((askGB * driveGBinMB) / driveMB);
-console.log('Nunbers of files:' + ' ' +  parseInt(numberOfFiles));
+const textNumbersOfFiles = 'Nunbers of files: ';
+console.log(textNumbersOfFiles  +  parseInt(numberOfFiles));
 
 
 // ***Норма***
 
 // Користувач вводить суму грошей в гаманці і ціну однієї шоколадки. Програма виводить скільки шоколадок може купити користувач і скільки здачі у нього залишиться.
-const askMoney = +prompt ('How much money you have available?');
+const askMoney = +prompt('How much money you have available?');
 const askPriceСhocolates = +prompt ('What is the price of a chocolate?');
 const numberOfChocolates = Math.floor(+askMoney / +askPriceСhocolates);
-console.log('Number of chocolate:' + ' ' +  numberOfChocolates);
+const textNumberChocolate = 'Number of chocolate: ';
+console.log(textNumberChocolate +  numberOfChocolates);
 
 const cash = (askMoney - numberOfChocolates * askPriceСhocolates);
-console.log('fund balance:' + ' ' +  cash.toFixed(2));
+const textBalance = 'Fund balance: ';
+console.log(textBalance + cash.toFixed(2));
 
 // Запитай у користувача тризначне число і виведи його задом наперед. Для вирішення завдання тобі знадобиться оператор % (залишок від ділення).
-const userNumber = +prompt ('Enter your number');
-const units = +userNumber % 10; 
-const tens = ~~(+userNumber / 10) % 10; 
-const hundreds = ~~(+userNumber / 100) % 10;
+const userNumber = +prompt('Enter your number');
+const units = userNumber % 10; 
+const tens = ~~(userNumber / 10) % 10; 
+const hundreds = ~~(userNumber / 100) % 10;
 const result = ((units * 100) + (tens * 10) + hundreds);
 console.log(result);
 
@@ -42,7 +45,8 @@ console.log(result);
 const askDeposit = +prompt('what is the amount of the deposit?');
 const percentDeposit = 0.05;
 const periodDepsit = 2;
-console.log('Amount of accrued annual interest:' + ' ' + ((askDeposit * (percentDeposit / 12)) * 2).toFixed(2));
+const textPercent = 'Amount of accrued annual interest: ';
+console.log(textPercent + ((askDeposit * (percentDeposit / 12)) * 2).toFixed(2));
 
 // Що повернуть вирази:
 console.log(2 && 0 && 3); // 0, оскільки логічний оператор && повертає false, а в даному прикладі false являється 0. 
