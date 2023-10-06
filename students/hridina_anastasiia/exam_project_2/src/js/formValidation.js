b578e3c5494f0c5da27741410bb4dfc06d3e1dec
Anastasia Hridina <homura.nyan@gmail.com>
2023-10-06 14:40:46 +0300

    added changes to footer

---
+++ students/hridina_anastasiia/exam_project_2/src/js/formValidation.js
@@ -0,0 +1,36 @@
+import JustValidate from "just-validate";
+
+function createFormValidation(form, getUserInfo) {
+
+
+    const validator = new JustValidate(form);
+
+    console.log(validator);
+    validator.addField('[name="fullname"]', [
+        {
+            rule: 'required'
+        },
+        {
+            rule: 'customRegexp',
+            value: /[a-z]/gi,
+        },
+        {
+            rule: 'minLength',
+            value: 3,
+        },
+    ]).addField('[name="email"]', [
+        {
+            rule: 'required'
+        },
+        {
+            rule: 'email',
+        }
+    ]);
+
+    validator.onSuccess((e) => {
+        getUserInfo(new FormData(e.target))
+    })
+}
+
+
+export default createFormValidation;
---
+++ students/hridina_anastasiia/exam_project_2/src/js/script.js
@@ -0,0 +1,185 @@
+import $ from 'jquery';
+import 'slick-carousel';
+import 'lightbox2';
+import button from './galleryButton.js';
+import createFormValidation from './formValidation.js';
+
+const addForm = document.querySelector('.form');
+
+createFormValidation(addForm, getUserInfo);
+
+function getUserInfo(data) {
+    const userInfo = Object.fromEntries([...data]);
+}
+
+
+$(".hero-slider").slick({
+    dots: true,
+    slidesToShow: 1,
+    slideToScroll: 1,
+    pauseOnHover: false,
+    verticalSwiping: false,
+    arrows: false,
+    vertical: true,
+    autoplay: false,
+    autoplaySpeed: 4000,
+    infinite: true
+});
+
+
+$('.article-wrapper').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    infinite: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true,
+                arrows: false
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true,
+                arrows: false
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true,
+                arrows: false
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true,
+                arrows: false,
+            }
+        },
+        {
+            breakpoint: 694,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+                arrows: false,
+            }
+        },
+    ]
+});
+
+lightbox.option({
+    fadeDuration: 600,
+    fitImagesInViewport: true,
+    imageFadeDuration: 600,
+    resizeDuration: 700,
+    wrapAround: false,
+    positionFromTop: 50,
+    disableScrolling: true,
+    showImageNumberLabel: false
+})
+
+
+const menu = $('.menu');
+let isOpenMenu = false;
+
+menu.on('transitioned', function () {
+    if (!isOpenMenu) {
+        menu.css({ display: '' });
+    }
+})
+
+$('.menu-open-btn').on('click', function (e) {
+    e.preventDefault();
+
+    if (!isOpenMenu) {
+        menu.css({ display: 'flex' });
+
+        setTimeout(function () {
+            $(this).closest('.menu-wrapper').toggleClass('_open', !isOpenMenu);
+
+            isOpenMenu = true;
+        }.bind(this), 0);
+
+        return;
+    }
+
+    $(this).closest('.menu-wrapper').toggleClass('_open', !isOpenMenu);
+
+    isOpenMenu = false;
+
+});
+
+const header = $('.header');
+const headerHeight = header.outerHeight(true);
+const secondSectionOffset = $('.two').offset().top;
+const offset = 20;
+const offsetBgHeader = secondSectionOffset - headerHeight - offset;
+
+let isHeaderBg = false;
+
+// $(window).on('scroll', function () {
+//     if ($(this).scrollTop() > offsetBgHeader && !isHeaderBg) {
+//         header.addClass('bg-header');
+//         isHeaderBg = true;
+//     } else if ($(this).scrollTop() < offsetBgHeader && !isHeaderBg) {
+//         header.removeClass('bg-header');
+//         isHeaderBg = false;
+//     };
+// });
+
+
+const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.25,
+};
+
+const callback = function (entries, observer) {
+    entries.forEach((entry) => {
+        if (entry.boundingClientRect.top < -20 && !entry.isIntersecting && !isHeaderBg) {
+            header.addClass('bg-header');
+            isHeaderBg = true;
+
+            return;
+        }
+
+        if (entry.boundingClientRect.top < -20 && entry.isIntersecting && isHeaderBg) {
+            header.removeClass('bg-header');
+            isHeaderBg = false;
+        }
+    })
+};
+
+const observer = new IntersectionObserver(callback, options);
+
+const target = document.querySelector(".hero");
+
+observer.observe(target);
+
+$('.scroll-to-top').on('click', function (e) {
+    e.preventDefault();
+
+    body.stop().animate({ scrollTop: 0 }, 200);
+});
+
