c7f7a92af874035ee8ad394c28e4a0c8d1da03f3
Viktor Dehtiarov <viktor.beet.root@gmail.com>
2023-10-09 23:30:47 +0300

    41. React intro

---
+++ main/frontEnd/41/src/js/App.js
@@ -0,0 +1,8 @@
+import { createElement } from "react";
+import Advice from "./components/Advice.js";
+
+function App() {
+    return createElement(Advice);
+}
+
+export default App;
---
+++ main/frontEnd/41/src/js/components/Advice.js
@@ -0,0 +1,37 @@
+import React, { createElement, useEffect, useState } from "react";
+
+export default function Advice() {
+    const [advice, setAdvice] = useState('');
+    const [counter, setCounter] = useState(0);
+
+    async function getAdvice() {
+        const res = await fetch('https://api.adviceslip.com/advice');
+        const data = await res.json();
+
+        setAdvice(data.slip.advice);
+        setCounter(counter + 1);
+    }
+
+    useEffect(
+        function () {
+            getAdvice();
+        },
+        []
+    );
+
+    // return createElement(
+    //     'div',
+    //     null,
+    //     createElement('h1', null, advice),
+    //     createElement('button', { onClick: getAdvice }, 'Отримати пораду'),
+    //     createElement('p', { className: 'message' }, `Ви переглянули ${counter} порад`)
+    // );
+
+    return (
+        <div>
+            <h1>{advice}</h1>
+            <button onClick={getAdvice}>Отримати пораду</button>
+            <p className="message">Ви переглянули {counter} порад</p>
+        </div>
+    );
+};
