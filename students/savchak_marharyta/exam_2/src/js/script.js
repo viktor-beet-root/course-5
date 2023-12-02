79b7439bf059fe311a10fb1709bd9728d319739a
Marharyta Savchak <marharyta.savchak04@gmail.com>
2023-12-02 05:37:20 +0200

    news_slider_done

--- students/savchak_marharyta/exam_2/src/js/script.js
+++ students/savchak_marharyta/exam_2/src/js/script.js
@@ -2,12 +2,13 @@ import $, { data } from 'jquery';
 import { Fancybox } from "@fancyapps/ui";
 import 'slick-carousel';
 
-
+import newsSlider from './slider/newsSlider.js';
 import heroSlider from './slider/heroSlider.js';
 import changeColorItemBg from './changeHeaderItemColor.js';
 import scrollMenuItem from './headerMenuScroll.js';
 
 
 heroSlider();
+newsSlider();
 changeColorItemBg();
 scrollMenuItem();
