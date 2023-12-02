3a384d1f9c50a617c002990bcf164bc99a56605d
Marharyta Savchak <marharyta.savchak04@gmail.com>
2023-12-02 05:40:04 +0200

    gallery_btn_done

--- students/savchak_marharyta/exam_2/src/js/script.js
+++ students/savchak_marharyta/exam_2/src/js/script.js
@@ -6,9 +6,11 @@ import newsSlider from './slider/newsSlider.js';
 import heroSlider from './slider/heroSlider.js';
 import changeColorItemBg from './changeHeaderItemColor.js';
 import scrollMenuItem from './headerMenuScroll.js';
+import btnGallery from './galleryBtn.js';
 
 
 heroSlider();
 newsSlider();
 changeColorItemBg();
 scrollMenuItem();
+btnGallery();
