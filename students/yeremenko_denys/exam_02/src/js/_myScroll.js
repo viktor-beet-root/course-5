c533b8f1a3fbdd3bb506383942124b4d9a64f83e
Denys Yeremenko <volchenka.denis@gmail.com>
2023-12-09 23:40:52 +0100

    first add

---
+++ students/yeremenko_denys/exam_02/src/js/_myScroll.js
@@ -0,0 +1,31 @@
+function myScroll() {
+
+    const sections = document.querySelectorAll("section");
+
+    function activeMenu() {
+
+        let length = sections.length;
+        while (--length && window.scrollY + 141 < sections[length].offsetTop) { };
+
+        const scrollTop = scrollY;
+        const header = document.querySelector('.header');
+        if (scrollTop >= 650) {
+            header.classList.add('hide');
+        } else {
+            header.classList.remove('hide');
+        };
+
+        const arrowTop = document.querySelector('.footer__go-top');
+        if (scrollTop >= 22) {
+            arrowTop.classList.add('hide-arrow');
+        } else {
+            arrowTop.classList.remove('hide-arrow');
+        };
+
+    };
+    activeMenu();
+    window.addEventListener("scroll", activeMenu);
+};
+
+
+export default myScroll;
